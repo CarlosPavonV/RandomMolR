33ed8711883ad10f19d805512b6b2d81f39eada0
CarlosPavonV <49054613+CarlosPavonV@users.noreply.github.com>
2023-07-12 13:43:45 -0400

    Update removeInv.R

--- removeInv.R
+++ removeInv.R
@@ -2,7 +2,23 @@ removeInv <- function(alignment,output){
   #Removes invariant sites from a FASTA alignment
   #alignment = character string with the name of the file containing the original alignment
   #output = character string with the name of the output file; e.g., "./output.fas"
+  require(stringr)
   seq.tmp <- readLines(alignment)
+  index.tmp <- which(str_detect(seq.tmp,">"))
+  if(index.tmp[2]-index.tmp[1]>2){
+    dnal.tmp <- c()
+    names.tmp <- seq.tmp[index.tmp]
+    for(i in 1:length(index.tmp)){
+      if(i < length(index.tmp)){
+        dna.tmp <- seq.tmp[(index.tmp[i]+1):(index.tmp[i+1]-1)]
+      } else {
+        dna.tmp <- seq.tmp[(index.tmp[i]+1):length(seq.tmp)]
+      }
+      dna.tmp <- paste(dna.tmp,collapse = "")
+      dnal.tmp <- c(dnal.tmp,dna.tmp)
+    }
+    seq.tmp <- c(rbind(names.tmp,dnal.tmp))
+  }
   dna.tmp <- seq.tmp[seq(2,length(seq.tmp),by=2)]
   dna.tmp <- sapply(dna.tmp,strsplit,split="")
   df.tmp <- matrix("A",ncol=length(dna.tmp[[1]]),nrow=length(dna.tmp))
@@ -10,7 +26,11 @@ removeInv <- function(alignment,output){
     df.tmp[i,] <- dna.tmp[[i]]
   }
   fun.tmp <- function(x){
-    x.tmp <- x[-which(x=="N" | x=="n" | x=="-" | x=="?")]
+    if("N" %in% x | "n" %in% x | "-" %in% x | "?" %in% x){
+      x.tmp <- x[-which(x=="N" | x=="n" | x=="-" | x=="?")]
+    } else {
+      x.tmp <- x
+    }
     l.tmp <- length(unique(x.tmp))
     if(l.tmp>1){
       return(FALSE)
